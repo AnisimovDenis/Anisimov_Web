a4c70d95e0411c4f0ebf22b7e0aa97dfaf76b6dd
Денис Анисимов <den.anisimov.94@mail.ru>
2020-09-07 17:43:38 +0300

    Создал контролер для студентов

---
+++ Anisimov/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Anisimov.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Anisimov.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly List<StudentViewModel> _students = new List<StudentViewModel>
+        {
+            new StudentViewModel
+            {
+                Id = 1,
+                FirstName = "Иван",
+                SurName = "Иванов",
+                Age = 22,
+                Class = 3
+            },
+            new StudentViewModel
+            {
+                Id = 2,
+                FirstName = "Владислав",
+                SurName = "Петров",
+                Age = 35,
+                Class = 4
+            }
+        };
+
+        public IActionResult Students()
+        {
+            return View(_students);
+        }
+    }
+}
