33ead1c05bf37323561d3b1ea96bb71b1329d50e
Денис Анисимов <den.anisimov.94@mail.ru>
2020-09-07 17:28:22 +0300

    Revert "Создал один интерфейс для всех классов InMemory"
    This reverts commit 2409d2b5c3ff5e947a10689f1f7c3074314e718e.

--- Anisimov/Infrastructure/Interfaces/IEmployeesService.cs
+++ Anisimov/Infrastructure/Interfaces/IEmployeesService.cs
@@ -6,5 +6,36 @@ using System.Threading.Tasks;
 
 namespace Anisimov.Infrastructure.Interfaces
 {
-    public interface IEmployeesService : IService<EmployeeViewModel> { }
+    public interface IEmployeesService
+    {
+        /// <summary>
+        /// Получение списка сотрудников
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<EmployeeViewModel> GetAll();
+
+        /// <summary>
+        /// Получение сотрудника по id
+        /// </summary>
+        /// <param name="id">Id</param>
+        /// <returns></returns>
+        EmployeeViewModel GetById(int id);
+
+        /// <summary>
+        /// Сохранить изменения
+        /// </summary>
+        void Commit();
+
+        /// <summary>
+        /// Добавить нового
+        /// </summary>
+        /// <param name="model"></param>
+        void AddNew(EmployeeViewModel model);
+
+        /// <summary>
+        /// Удалить
+        /// </summary>
+        /// <param name="id"></param>
+        void Delete(int id);
+    }
 }
