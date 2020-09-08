2ed1a7b47a957653ef2e1e2891ac05a2865b7da4
Денис Анисимов <den.anisimov.94@mail.ru>
2020-09-08 12:22:15 +0300

    Добавил Action методы в StudentController

--- Anisimov/Controllers/StudentController.cs
+++ Anisimov/Controllers/StudentController.cs
@@ -21,5 +21,61 @@ namespace Anisimov.Controllers
         {
             return View(this.studentsService.GetAll());
         }
+        [Route("{id}")]
+        public IActionResult EmployeeDetails(int id)
+        {
+            var employee = this.studentsService.GetById(id);
+
+            if (employee == null)
+                return NotFound();
+
+            return View(employee);
+        }
+
+        [HttpGet]
+        [Route("edit/{id?}")]
+        public IActionResult Edit(int? id)
+        {
+            if (!id.HasValue)
+                return View(new EmployeeViewModel());
+
+            var model = this.studentsService.GetById(id.Value);
+            if (model == null)
+                return NotFound();// возвращаем результат 404 Not Found
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [Route("edit/{id?}")]
+        public IActionResult Edit(StudentViewModel model)
+        {
+            if (model.Id > 0) // если есть Id, то редактируем модель
+            {
+                var dbItem = this.studentsService.GetById(model.Id);
+
+                if (ReferenceEquals(dbItem, null))
+                    return NotFound();// возвращаем результат 404 Not Found
+
+                dbItem.FirstName = model.FirstName;
+                dbItem.SurName = model.SurName;
+                dbItem.Age = model.Age;
+                dbItem.Class = model.Class;
+            }
+            else // иначе добавляем модель в список
+            {
+                this.studentsService.AddNew(model);
+            }
+            this.studentsService.Commit(); // станет актуальным позднее (когда добавим БД)
+
+            return RedirectToAction(nameof(Students));
+        }
+
+        [Route("delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            this.studentsService.Delete(id);
+            return RedirectToAction(nameof(Students));
+        }
     }
 }
